10c18ff92e2590efc5553825b8811029558114ca
mfrank.23 <mfrank.23@57db3241-4b5c-441e-8149-1640c572df1c>
2010-05-29 06:19:17 +0000

    files of test class Account

---
+++ Account.cpp
@@ -0,0 +1,8 @@
+#include "Account.h"
+
+using namespace std;
+
+Account::Account(char *acc_no, float b){
+  account_no=acc_no;
+  balance=b;
+}
---
+++ Account.h
@@ -0,0 +1,17 @@
+
+
+class Account {
+
+ private:
+  char *account_no;
+  float balance;
+
+ public:
+  Account(char *acc_no, float b) {
+    account_no=acc_no;
+    balance=b;
+
+
+  }
+
+};
