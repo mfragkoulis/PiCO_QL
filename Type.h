4512e8d85dd58011502155afcf29226da3c3147e
mfrank.23 <mfrank.23@57db3241-4b5c-441e-8149-1640c572df1c>
2010-06-01 13:40:34 +0000

    Type class

---
+++ Type.h
@@ -0,0 +1,27 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+class Type {
+
+ private:
+  char *text;
+  float doub;
+  int integ;
+  void *blob;
+  int type_of_value;
+
+ public:
+  Type();
+  void set_int(int i);
+  void set_doub(float f);
+  void set_text(const unsigned char * t);
+  void set_blob(const void * b);
+  void set_type_of_value(int tov);
+  int get_type_of_value();
+  float get_doub();
+  char * get_text();
+
+};
+
+
+#endif
