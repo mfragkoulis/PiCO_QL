50eecc40e0290081b3b403256a84a156181ff684
mfrank.23 <mfrank.23@57db3241-4b5c-441e-8149-1640c572df1c>
2012-02-08 09:11:28 +0000

    Filling in BankApp example.

---
+++ EXAMPLES/BankApp/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string>
+#include <map>
+#include "SpecialAccount.h"
+#include "Account.h"
+#include "SuperAccount.h"
+
+using namespace std;
+
+int main(){
+    
+    Account acc1("10068", 500.0, 478923743, -13);
+    Account acc2("10234", 394.28, 692346729, -25);
+    SpecialAccount spa1("23999", 1000.00, 639269243, 34);
+    SpecialAccount spa2("69263", 131.00, 197396445, 72);
+    SuperAccount sa1(39);
+    SuperAccount sa2(91);
+    map<string,SuperAccount> accounts;
+    map<string,SuperAccount>::iterator it;
+    accounts.insert(make_pair("0", acc1));
+    accounts.insert(make_pair("1", acc2));
+    accounts.insert(make_pair("2", spa1));
+    accounts.insert(make_pair("3", spa2));
+    accounts.insert(make_pair("4", sa1));
+    accounts.insert(make_pair("5", sa2));
+    
+    for (it=accounts.begin(); it != accounts.end();it++) {
+	printf("%i\n", it->second.get_account_no());
+	
+    }
+
+}
