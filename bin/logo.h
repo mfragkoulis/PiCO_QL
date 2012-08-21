478522c0da376cdae741e6ba9d5b99fcd24b2c76
Marios Fragkoulis <mfg@aueb.gr>
2012-08-21 14:23:47 +0300

    Added logo.h for drawing pico_ql logo as a byte array.

---
+++ bin/logo.h
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2004-2009 Diomidis Spinellis.  All rights reserved.
+ *
+ * You may only use this code if you agree to the terms of the CScout
+ * Source Code License agreement (see License.txt).
+ * If you do not agree to the terms, do not use the code.
+ *
+ * The CScout logo access functions
+ *
+ * $Id: logo.h 1.3 2009/01/15 14:32:57 dds Exp $
+ */
+
+#ifndef LOGO_H
+#define LOGO_H
+
+void logo(FILE *fo);
+
+#endif // LOGO
