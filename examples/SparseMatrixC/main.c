ea3334d2288573135cb690944f307fd22d71f837
Marios Fragkoulis <mfg@aueb.gr>
2013-04-22 15:48:06 +0300

    Corrected bug in ruby template concerning view definition generation for C binding. Also enhanced sparse matrix example.

--- examples/SparseMatrixC/main.c
+++ examples/SparseMatrixC/main.c
@@ -6,48 +6,37 @@
 
 int main() {
   struct denseMatrix dm;
-  dm.size = 6;
+  int r = 0, c = 0, n = 0;
+  int rows = 3;
+  int cols = 4;
+  int matrix[3][4] = {{1, 2, 0, 0},
+                      {0, 3, 9, 0},
+                      {0, 1, 4, 0}};
+  dm.size = 0;
+
+  for (r = 0; r < rows; r++) {
+    for (c = 0; c < cols; c++) {
+      if (matrix[r][c] > 0)
+        dm.size++;
+      printf("matrix[%i][%i] = %i\n", r, c, matrix[r][c]);
+    }
+    printf("\n");
+  }
   dm.me = (struct matrixElement *)malloc(sizeof(struct matrixElement) * dm.size);
-  dm.me[0].row = 0;
-  dm.me[0].col = 0;
-  dm.me[0].val = 1;
-
-  dm.me[1].row = 0;
-  dm.me[1].col = 1;
-  dm.me[1].val = 2;
-
-  dm.me[2].row = 1;
-  dm.me[2].col = 1;
-  dm.me[2].val = 3;
-
-  dm.me[3].row = 1;
-  dm.me[3].col = 2;
-  dm.me[3].val = 9;
-
-  dm.me[4].row = 2;
-  dm.me[4].col = 1;
-  dm.me[4].val = 1;
-
-  dm.me[5].row = 2;
-  dm.me[5].col = 2;
-  dm.me[5].val = 4;
+  for (r = 0; r < rows; r++) {
+    for (c = 0; c < cols; c++) {
+      if (matrix[r][c] > 0) {
+        dm.me[n].row = r + 1;
+        dm.me[n].col = c + 1;
+        dm.me[n].val = matrix[r][c];
+        n++;
+      }
+    }
+  }
 
   pico_ql_register(&dm, "denseMatrix");
   pico_ql_serve(8080);
 
-  int r = 0, c = 0;
-  int rows = 3;
-  int cols = 4;
-//  int **matrix = (int **)malloc(rows * (sizeof(int) * cols));
-  int matrix[3][4] = {{1, 2, 0, 0},
-                      {0, 3, 9, 0},
-                      {0, 1, 4, 0}};
-
-  for (r = 0; r < rows; r++) {
-    for (c = 0; c < cols; c++)
-      printf("matrix[%i][%i] = %i\n", r, c, matrix[r][c]);
-    printf("\n");
-  }
   return 0;
 }
 
--- examples/SparseMatrixC/pico_ql_dsl.sql
+++ examples/SparseMatrixC/pico_ql_dsl.sql
@@ -14,3 +14,23 @@ USING STRUCT VIEW Matrix
 WITH REGISTERED C NAME denseMatrix
 WITH REGISTERED C TYPE struct denseMatrix:struct matrixElement *
 USING LOOP for(iter = &base->me[record]; record != base->size; Matrix_advance(iter,base->me,++record))$
+
+CREATE VIEW RowWiseA AS 
+SELECT mval, mcol 
+FROM Matrix 
+WHERE mval != 0 
+ORDER BY mrow, mcol$
+
+CREATE VIEW RowWiseB AS 
+SELECT mrow, COUNT(mrow) AS countRows
+FROM Matrix 
+WHERE mval != 0 
+GROUP BY mrow 
+ORDER BY mrow$
+
+CREATE VIEW RowWiseC AS
+SELECT B1.mrow, IFNULL(SUM(B2.countRows), 0) + 1 as IA 
+FROM RowWiseB B1 
+LEFT OUTER JOIN RowWiseB B2 
+ON B2.mrow < B1.mrow 
+GROUP BY B1.mrow$
