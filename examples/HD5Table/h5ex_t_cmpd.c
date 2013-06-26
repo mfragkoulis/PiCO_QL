8961d3233a157a0ed7b30649475a027113a6b8eb
Marios Fragkoulis <mfg@aueb.gr>
2013-06-26 21:19:18 +0300

    Experimenting with HDF5 multidimensional arrays. Work in progress.

--- examples/HD5Table/h5ex_t_cmpd.c
+++ examples/HD5Table/h5ex_t_cmpd.c
@@ -19,6 +19,8 @@
 #define FILE            "h5ex_t_cmpd.h5"
 #define DATASET         "DS1"
 #define DIM0            4
+#define DIM1            3
+#define DIM2            2
 
 
 int
@@ -27,31 +29,116 @@ main (void)
     hid_t       file, filetype, memtype, strtype, space, dset;
                                             /* Handles */
     herr_t      status;
-    hsize_t     dims[1] = {DIM0};
-    sensor_t    wdata[DIM0],                /* Write buffer */
-                *rdata;                     /* Read buffer */
+    hsize_t     dims[3] = {DIM0, DIM1, DIM2};
+    sensor_t    wdata[DIM0][DIM1][DIM2],                /* Write buffer */
+                ***rdata;                     /* Read buffer */
     int         ndims,
-                i;
+                i, j, k;
 
     /*
      * Initialize data.
      */
-    wdata[0].serial_no = 1153;
-    wdata[0].location = "Exterior (static)";
-    wdata[0].temperature = 53.23;
-    wdata[0].pressure = 24.57;
-    wdata[1].serial_no = 1184;
-    wdata[1].location = "Intake";
-    wdata[1].temperature = 55.12;
-    wdata[1].pressure = 22.95;
-    wdata[2].serial_no = 1027;
-    wdata[2].location = "Intake manifold";
-    wdata[2].temperature = 103.55;
-    wdata[2].pressure = 31.23;
-    wdata[3].serial_no = 1313;
-    wdata[3].location = "Exhaust manifold";
-    wdata[3].temperature = 1252.89;
-    wdata[3].pressure = 84.11;
+    wdata[0][0][0].serial_no = 1153;
+    wdata[0][0][0].location = "Exterior (static)";
+    wdata[0][0][0].temperature = 53.23;
+    wdata[0][0][0].pressure = 24.57;
+    wdata[1][0][0].serial_no = 1184;
+    wdata[1][0][0].location = "Intake";
+    wdata[1][0][0].temperature = 55.12;
+    wdata[1][0][0].pressure = 22.95;
+    wdata[2][0][0].serial_no = 1027;
+    wdata[2][0][0].location = "Intake manifold";
+    wdata[2][0][0].temperature = 103.55;
+    wdata[2][0][0].pressure = 31.23;
+    wdata[3][0][0].serial_no = 1313;
+    wdata[3][0][0].location = "Exhaust manifold";
+    wdata[3][0][0].temperature = 1252.89;
+    wdata[3][0][0].pressure = 84.11;
+
+    wdata[0][1][0].serial_no = 1153;
+    wdata[0][1][0].location = "Exterior (static)";
+    wdata[0][1][0].temperature = 53.23;
+    wdata[0][1][0].pressure = 24.57;
+    wdata[1][1][0].serial_no = 1184;
+    wdata[1][1][0].location = "Intake";
+    wdata[1][1][0].temperature = 55.12;
+    wdata[1][1][0].pressure = 22.95;
+    wdata[2][1][0].serial_no = 1027;
+    wdata[2][1][0].location = "Intake manifold";
+    wdata[2][1][0].temperature = 103.55;
+    wdata[2][1][0].pressure = 31.23;
+    wdata[3][1][0].serial_no = 1313;
+    wdata[3][1][0].location = "Exhaust manifold";
+    wdata[3][1][0].temperature = 1252.89;
+    wdata[3][1][0].pressure = 84.11;
+
+    wdata[0][2][0].serial_no = 1153;
+    wdata[0][2][0].location = "Exterior (static)";
+    wdata[0][2][0].temperature = 53.23;
+    wdata[0][2][0].pressure = 24.57;
+    wdata[1][2][0].serial_no = 1184;
+    wdata[1][2][0].location = "Intake";
+    wdata[1][2][0].temperature = 55.12;
+    wdata[1][2][0].pressure = 22.95;
+    wdata[2][2][0].serial_no = 1027;
+    wdata[2][2][0].location = "Intake manifold";
+    wdata[2][2][0].temperature = 103.55;
+    wdata[2][2][0].pressure = 31.23;
+    wdata[3][2][0].serial_no = 1313;
+    wdata[3][2][0].location = "Exhaust manifold";
+    wdata[3][2][0].temperature = 1252.89;
+    wdata[3][2][0].pressure = 84.11;
+
+    wdata[0][0][1].serial_no = 1153;
+    wdata[0][0][1].location = "Exterior (static)";
+    wdata[0][0][1].temperature = 53.23;
+    wdata[0][0][1].pressure = 24.57;
+    wdata[1][0][1].serial_no = 1184;
+    wdata[1][0][1].location = "Intake";
+    wdata[1][0][1].temperature = 55.12;
+    wdata[1][0][1].pressure = 22.95;
+    wdata[2][0][1].serial_no = 1027;
+    wdata[2][0][1].location = "Intake manifold";
+    wdata[2][0][1].temperature = 103.55;
+    wdata[2][0][1].pressure = 31.23;
+    wdata[3][0][1].serial_no = 1313;
+    wdata[3][0][1].location = "Exhaust manifold";
+    wdata[3][0][1].temperature = 1252.89;
+    wdata[3][0][1].pressure = 84.11;
+
+    wdata[0][1][1].serial_no = 1153;
+    wdata[0][1][1].location = "Exterior (static)";
+    wdata[0][1][1].temperature = 53.23;
+    wdata[0][1][1].pressure = 24.57;
+    wdata[1][1][1].serial_no = 1184;
+    wdata[1][1][1].location = "Intake";
+    wdata[1][1][1].temperature = 55.12;
+    wdata[1][1][1].pressure = 22.95;
+    wdata[2][1][1].serial_no = 1027;
+    wdata[2][1][1].location = "Intake manifold";
+    wdata[2][1][1].temperature = 103.55;
+    wdata[2][1][1].pressure = 31.23;
+    wdata[3][1][1].serial_no = 1313;
+    wdata[3][1][1].location = "Exhaust manifold";
+    wdata[3][1][1].temperature = 1252.89;
+    wdata[3][1][1].pressure = 84.11;
+
+    wdata[0][2][1].serial_no = 1153;
+    wdata[0][2][1].location = "Exterior (static)";
+    wdata[0][2][1].temperature = 53.23;
+    wdata[0][2][1].pressure = 24.57;
+    wdata[1][2][1].serial_no = 1184;
+    wdata[1][2][1].location = "Intake";
+    wdata[1][2][1].temperature = 55.12;
+    wdata[1][2][1].pressure = 22.95;
+    wdata[2][2][1].serial_no = 1027;
+    wdata[2][2][1].location = "Intake manifold";
+    wdata[2][2][1].temperature = 103.55;
+    wdata[2][2][1].pressure = 31.23;
+    wdata[3][2][1].serial_no = 1313;
+    wdata[3][2][1].location = "Exhaust manifold";
+    wdata[3][2][1].temperature = 1252.89;
+    wdata[3][2][1].pressure = 84.11;
 
     /*
      * Create a new file using the default properties.
@@ -95,7 +182,7 @@ main (void)
      * Create dataspace.  Setting maximum size to NULL sets the maximum
      * size to be the current size.
      */
-    space = H5Screate_simple (1, dims, NULL);
+    space = H5Screate_simple (3, dims, NULL);
 
     /*
      * Create the dataset and write the compound data to it.
@@ -134,22 +221,36 @@ main (void)
      */
     space = H5Dget_space (dset);
     ndims = H5Sget_simple_extent_dims (space, dims, NULL);
-    rdata = (sensor_t *) malloc (dims[0] * sizeof (sensor_t));
+    rdata = (sensor_t ***) malloc (dims[0] * sizeof (sensor_t**));
+    for (j=0; j<dims[0]; j++) {
+      rdata[j] = (sensor_t **) malloc (dims[1] * sizeof (sensor_t*));
+      for (k=0; k<dims[1]; k++) {
+        rdata[j][k] = (sensor_t *) malloc (dims[2] * sizeof (sensor_t));
+      }
+    }
 
     /*
      * Read the data.
      */
-    status = H5Dread (dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+//    status = H5Dread (dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
 
     /*
      * Output the data to the screen.
      */
+    printf("*** %lx\n", (long unsigned int)rdata);
     for (i=0; i<dims[0]; i++) {
-        printf ("%s[%d]:\n", DATASET, i);
-        printf ("Serial number   : %d\n", rdata[i].serial_no);
-        printf ("Location        : %s\n", rdata[i].location);
-        printf ("Temperature (F) : %f\n", rdata[i].temperature);
-        printf ("Pressure (inHg) : %f\n\n", rdata[i].pressure);
+      printf("** %lx\n", (long unsigned int)rdata[i]);
+      for (j=0; j<dims[1]; j++) {
+        printf("* %lx\n", (long unsigned int)rdata[i][j]);
+        for (k=0; k<dims[2]; k++) {
+          printf("%lx\n", (long unsigned int)&rdata[i][j][k]);
+          printf ("%s[%d][%d][%d]:\n", DATASET, i, j, k);
+          //printf ("Serial number   : %d\n", rdata[i][j][k].serial_no);
+          //printf ("Location        : %s\n", rdata[i][j][k].location);
+          //printf ("Temperature (F) : %f\n", rdata[i][j][k].temperature);
+          //printf ("Pressure (inHg) : %f\n\n", rdata[i][j][k].pressure);
+        }
+      }
     }
 
     /*
