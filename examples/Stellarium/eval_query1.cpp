60171ef8d084dea36ee7bf67356aaba4a0a92bdc
Marios Fragkoulis <mfg@aueb.gr>
2013-07-23 13:02:32 +0300

    Improved Stellarium's and QLandKarte's CPP evaluation queries.

--- examples/Stellarium/eval_query1.cpp
+++ examples/Stellarium/eval_query1.cpp
@@ -1,42 +1,33 @@
-void cpp_query1() {                                         
-  clock_t start, finish;                                    
-  double t = 0;                                             
-  std::list<PlanetP>::iterator iter;                        
-  std::set<double> resultsetP;                              
-  double min_distance;                                      
-  std::vector<Meteor*>* stdMet;                             
-  std::vector<Meteor*>::iterator it;                        
-  for (int i = 0; i< 100; i++) {                            
-    start = clock();                                        
-    *stdSystemPlanets = stdSolar->getAllPlanets().toStdList();
-    for (iter = stdSystemPlanets->begin(); iter != stdSystemPlanets->end(); iter++) {                                  
-      if (strcmp((*iter).data()->getNameI18n().toStdString().c_str(), "Earth"))                                        
-        resultsetP.insert((*iter).data()->getDistance());   
-    }                                                       
-    min_distance = *min_element(resultsetP.begin(), resultsetP.end());                                                 
-    stdMet = stdMeteors->getActive();                       
-    if (stdMet->size() > 0) {                               
-      std::cout << "Min distance is:: " << min_distance   
-                << std::endl;                               
-      std::cout << "observdistance | velocity | "           
-                << " magnitude | scalemagnitude "           
-                << std::endl;                               
-    }                                                       
-    for (it = stdMet->begin(); it != stdMet->end(); it++) {
-      if (((*it)->xydistance > min_distance) && ((*it)->alive)) {                                                      
-        std::cout << (*it)->xydistance << " | "             
-                  << (*it)->velocity << " | "               
-                  << (*it)->mag << " | "                    
-                  << (*it)->distMultiplier                  
-                  << std::endl;                             
-      }                                                     
-    }                                                       
-    finish = clock();                                       
-    t += ((double)finish - (double)start)/CLOCKS_PER_SEC;   
-  }                                                         
-  if (stdMet->size() > 0) {                                 
-      std::cout << "Query took " << t/100                   
-                << " seconds to execute."                   
-                << std::endl;                               
-  }                                    
-}
+/* C++ lines of code: 14
+ * x: excluded
+ */
+void cpp_query1() {
+  PlanetP iter;
+  std::set<double> resultsetP;
+  double min_distance;
+  std::vector<Meteor*>::iterator it;
+  for (int i = 0; i< 100; i++) {                      /*  x */
+    foreach (iter,  solar->getAllPlanets()) {
+      if (strcmp(iter.data()->getNameI18n().toStdString().c_str(), "Earth"))
+        resultsetP.insert(iter.data()->getDistance());
+    }                                                 /*  x */
+    min_distance = *min_element(resultsetP.begin(), resultsetP.end());
+    if (meteor->getActive() && meteor->getActive()->size() > 0) {
+      std::cout << "Min distance is:: " << min_distance
+                << std::endl;                         /*  x */
+      std::cout << "observdistance | velocity | "     /*  x */
+                << " magnitude | scalemagnitude "     /*  x */
+                << std::endl;                         /*  x */
+      for (it = meteor->getActive()->begin(); it != meteor->getActive()->end(); it++) {
+        if (((*it)->xydistance > min_distance) && ((*it)->alive)) {
+          std::cout << (*it)->xydistance << " | "
+                    << (*it)->velocity << " | "       /*  x */
+                    << (*it)->mag << " | "            /*  x */
+                    << (*it)->distMultiplier          /*  x */
+                    << std::endl;                     /*  x */
+        }                                             /*  x */
+      }                                               /*  x */
+    }                                                 /*  x */
+  }                                                   /*  x */
+}                                                     /*  x */
+
--- examples/Stellarium/eval_query3.cpp
+++ examples/Stellarium/eval_query3.cpp
@@ -1,46 +1,39 @@
+/* C++ lines of code: 20
+ * x: excluded
+ */
 void cpp_query3(SolarSystem *s) {                                         
-  clock_t start, finish;                                    
-  double t = 0;                                             
   PlanetP iterP, iterSP;
   std::multimap<std::string, std::pair<int,Planet *> > aggregate;
   std::pair<int, Planet *> pSatellites;                     
   std::multimap<std::string, std::pair<int, Planet *> >::iterator aggrIt;
   int count = 0;                                            
   Planet *currentP;                                         
-  for (int i = 0; i < 100; i++) {                           
-    start = clock();                                        
-    count = 0;                                              
+  for (int i = 0; i < 100; i++) {                       /*  x */  
     foreach (iterP, s->getAllPlanets()) {                              
       count = 0;                                            
       if (StelApp::getInstance().getCore()->getProjection(StelApp::getInstance().getCore()->getHeliocentricEclipticModelViewTransform())->checkInViewport(iterP->screenPos) {
         foreach (iterSP, (*iterP).satellites()) {
         if ((*iterSP).hasAtmosphere())              
             count++;                                          
-        }
+        }                                               /*  x */  
         if (count > 0) {                                      
           pSatellites = std::make_pair (count, iterP.data());
           aggregate.insert(std::pair<std::string, std::pair<int, Planet*>>((*iterP).getNameI18n().toStdString(), pSatellites));
-        }
-      }                                                     
-    }                                                       
+        }                                               /*  x */  
+      }                                                 /*  x */      
+    }                                                   /*  x */      
     std::cout << " name | radius | "                        
-              << "period | albedo | NoSatellitesAtm "       
-              << std::endl;                                 
+              << "period | albedo | NoSatellitesAtm "   /*  x */      
+              << std::endl;                             /*  x */      
     for (aggrIt = aggregate.begin(); aggrIt != aggregate.end(); aggrIt++) {
       currentP = (*aggrIt).second.second;                   
       std::cout << (*aggrIt).first << " | "                 
-                << currentP->getRadius() << " | "           
-                << currentP->getSiderealDay() << " | "      
-                << currentP->albedo << " | "                
-                << (*aggrIt).second.first                   
-                << std::endl;                               
-    }                                                       
-    aggregate.clear();                                      
-    finish = clock();                                       
-    t += ((double)finish - (double)start)/CLOCKS_PER_SEC;   
-  }                                                         
-  t = t/100;                                                
-  std::cout << "Query took " << t                           
-            << " seconds to execute."                       
-            << std::endl;                                   
-}
+                << currentP->getRadius() << " | "       /*  x */      
+                << currentP->getSiderealDay() << " | "  /*  x */      
+                << currentP->albedo << " | "            /*  x */      
+                << (*aggrIt).second.first               /*  x */      
+                << std::endl;                           /*  x */      
+    }                                                   /*  x */      
+    aggregate.clear();                                  /*  x */      
+  }                                                     /*  x */      
+}                                                       /*  x */  
