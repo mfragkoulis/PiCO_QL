1c8d962fab5495ffedcf8518cc01922735d2e287
Marios Fragkoulis <mfg@aueb.gr>
2013-11-04 15:02:01 +0200

    Trivial fix for USING LOOP on CPP containers.

--- examples/CApp/test.sh
+++ examples/CApp/test.sh
@@ -7,7 +7,7 @@ ruby pico_ql_generator.rb pico_ql_dsl.sql C
 # Add "G_CXX=1" to compile options for C++ build
 # Available test configurations (2):
 #make PICO_QL_DEBUG=1 PICO_QL_JOIN_THREADS=1
-make PICO_QL_SINGLE_THREADED=1 
+make PICO_QL_SINGLE_THREADED=1
 # Available release (SWILL interface) configurations (2):
 #make RELEASE=1 PICO_QL_SINGLE_THREADED=1
 #make RELEASE=1 PICO_QL_JOIN_THREADS=1
